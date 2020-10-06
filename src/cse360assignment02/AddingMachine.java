efadf2b5ede5b3e761aa2d202a0c02d05b8f44e4
Cullen617 <70451682+Cullen617@users.noreply.github.com>
2020-10-05 20:36:49 -0700

    edit and add comments

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,10 +1,24 @@
+/**
+ * Written by: Cullen Walsh
+ * id:1214938374
+ * date:10/5/2020
+ * CSE 360
+ *
+ * AddingMachine.java: The AddingMachine Class will create a simple calculator
+ * that will compute addition and subtraction to the value of total. While Also
+ * keeping a transaction list of every operation that is calculated against total.
+ */
+
+
+
 package cse360assignment02;
 
-public class AddingMachine {private int total;
-private String transactions;
+public class AddingMachine {
+    private int total;
+    private String transactions;
 
     /**
-     * AddingMachine constructor initializing int total
+     * AddingMachine constructor initializing int total and transactions
      */
 
     public AddingMachine () {
@@ -14,7 +28,7 @@ private String transactions;
 
     /**
      * getTotal method returning the total variable
-     * @return
+     * @return the value that is stored in total
      */
     public int getTotal () {
         return total;
@@ -22,7 +36,7 @@ private String transactions;
 
     /**
      * add method that will add value to the total variable
-     * @param value
+     * @param value which is then added to the total
      */
     public void add (int value) {
         total = total + value;
@@ -32,7 +46,7 @@ private String transactions;
 
     /**
      * subtract method that will subtract value from total variable
-     * @param value
+     * @param value which is then subtracted from the total
      */
     public void subtract (int value) {
         total = total - value;
@@ -41,6 +55,7 @@ private String transactions;
 
     /**
      * toString method that will return all the operation done to total
+     * @return the string holding a list of all operation done to total
      */
     public String toString () {
 
@@ -48,7 +63,7 @@ private String transactions;
     }
 
     /**
-     * clear method that will clear used memory
+     * clear method that will clear used memory in the variables total and transactions
      */
     public void clear() {
         total = 0;
