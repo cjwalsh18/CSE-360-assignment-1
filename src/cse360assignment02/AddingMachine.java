e45064d8f4e9502279c684512554dcbec4d55cba
Cullen617 <70451682+Cullen617@users.noreply.github.com>
2020-10-05 19:11:57 -0700

    initial commit for main and AddingMachine class as well as comments.

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+
+public class AddingMachine {private int total;
+
+    /**
+     * AddingMachine constructor initializing int total
+     */
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * getTotal method returning the total variable
+     * @return
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * add method that will add value to the total variable
+     * @param value
+     */
+    public void add (int value) {
+
+    }
+
+    /**
+     * subtract method that will subtract value from total variable
+     * @param value
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * toString method that will return all the operation done to total
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * clear method that will clear used memory
+     */
+    public void clear() {
+    }
+}
