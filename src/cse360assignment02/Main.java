8d231a30fd4bec229be535c16d2bd86cf2cb59e6
Cullen617 <70451682+Cullen617@users.noreply.github.com>
2020-10-05 20:28:41 -0700

    Setting up test cases in Main class

--- src/cse360assignment02/Main.java
+++ src/cse360assignment02/Main.java
@@ -3,6 +3,18 @@ package cse360assignment02;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int num = 5;
+	int num2 = 10;
+	int num3 = 3;
+	AddingMachine myCalc = new AddingMachine();
+	myCalc.add(5);
+	myCalc.add(num2);
+	myCalc.subtract(num3);
+
+	System.out.println(myCalc.toString());
+	System.out.println(myCalc.getTotal());
+	myCalc.clear();
+	System.out.println(myCalc.toString());
+	System.out.println(myCalc.getTotal());
     }
 }
